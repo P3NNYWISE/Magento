04b88a2f314951018873a526c3d5039bf48988c7
fabio <fabio.ger@gmail.com>
2016-03-22 16:44:12 -0300

    section 5

---
+++ app/code/local/MasteringMagento/Example/Model/Product/Type/Event.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * app/code/local/MasteringMagento/Model/Project/Type/Event.php
+ *
+ * This example code is provided for use with the Mastering Magento video
+ * series, by Packt Publishing.
+ *
+ * @author    Franklin P. Strube <franklin.strube@gmail.com>
+ * @category  MasteringMagento
+ * @package   Example
+ * @copyright Copyright (c) 2012 Packt Publishing (http://packtpub.com)
+ */
+class MasteringMagento_Example_Model_Product_Type_Event extends Mage_Catalog_Model_Product_Type_Abstract
+{
+}
