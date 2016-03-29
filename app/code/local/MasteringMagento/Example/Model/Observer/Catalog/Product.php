947557bf22caf5ddac3c061061897891742ce949
fabio <fabio.ger@gmail.com>
2016-03-29 13:23:56 -0300

    6.1

---
+++ app/code/local/MasteringMagento/Example/Model/Observer/Catalog/Product.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * app/code/local/MasteringMagento/Example/Model/Observer/Catalog/Product.php
+ *
+ * This example code is provided for use with the Mastering Magento video
+ * series, by Packt Publishing.
+ *
+ * @author    Franklin P. Strube <franklin.strube@gmail.com>
+ * @category  MasteringMagento
+ * @package   Example
+ * @copyright Copyright (c) 2012 Packt Publishing (http://packtpub.com)
+ */
+class MasteringMagento_Example_Model_Observer_Catalog_Product
+{
+    // TODO follow along with the video!
+    public function getFinalPrice($observer)
+    {
+    	$product = $observer->getEvent()->getProduct();
+    	if ($buyRequest = $product->getCustomOption('info_buyRequest')){
+    		$buyRequest = new Varien_Object(unserialize($buyRequest->getValue()));
+    		$price = $product->getFinalPrice();
+    		if ($tickets = $buyRequest->getTicket()){
+    			foreach ($tickets as $ticketId => $data) {
+    				$_ticket = Mage::getModel('example/event_ticket')->load($ticketId);
+    				$price += $data['qty'] * $_ticket->getPrice();
+    				# code...
+    			}
+    		}
+    		$product->setFinalPrice();
+    	}
+    	return $this;
+    }
+}
+	
